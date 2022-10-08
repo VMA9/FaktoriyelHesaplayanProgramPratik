7a39c5cf91c534ccfc6718742175e33046854c66
Muhammet Ali Velibeyoğlu <111783796+VMA9@users.noreply.github.com>
2022-10-09 01:00:27 +0300

    Update FaktoriyelHesaplayanProgramPratik.java

--- src/FaktoriyelHesaplayanProgramPratik.java
+++ src/FaktoriyelHesaplayanProgramPratik.java
@@ -1,4 +1,11 @@
 import java.util.Scanner;
+/* Ödev   N elemanlı bir kümenin elemanları ile oluşturulacak r elemanlı farklı grupların sayısı n’in r’li kombinasyonu olarak adlandırılır. N’in r’li kombinasyonu C(n,r) şeklinde gösterilir.
+
+Java ile kombinasyon hesaplayan program yazınız.
+
+Kombinasyon formülü
+C(n,r) = n! / (r! * (n-r)!)
+*/
 
 public class FaktoriyelHesaplayanProgramPratik {
     public static void main(String[] args) {
